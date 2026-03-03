b8ce8c4e9a83b44dba48fe1a68f87f776d113ede
Masoud Ghanbari <masoud.ghanbari@adevinta.com>
2026-03-03 12:06:45 +0100

    updates about me

--- src/pages/home/about-content.ts
+++ src/pages/home/about-content.ts
@@ -5,7 +5,8 @@ export const aboutContent = {
     paragraphs: [
       "Hey there! Analytics guy here! 👋",  
       "I currently work at Marktplaats (yes, the Dutch eBay) as a Analytics Engineering Tech Lead.",
-      "My superpower? I'm always up for the game. I believe playing games helps making great teams and awesome teams build great products. \n",
+      "My superpower? I'm always up for the game.",
+      "I believe playing games helps making great teams and awesome teams build great products.",
       ]
   }
 };
