058fd673155d9950196695815f71e0087e2bc4b0
Masoud Ghanbari <masoud.ghanbari@adevinta.com>
2026-03-06 22:52:25 +0100

    fix: update about content

--- src/pages/home/about-content.ts
+++ src/pages/home/about-content.ts
@@ -3,7 +3,7 @@ export const aboutContent = {
   bio: {
     title: "",
     paragraphs: [
-      "Hey there! Analytics guy here! 👋",  
+      "Hey there! This is Massoud, aka the Analytics guy! 👋",  
       "I currently work at Marktplaats (yes, the Dutch eBay) as a Analytics Engineering Tech Lead.",
       "My superpower? I'm always up for the game.",
       "I believe playing games helps making great teams and awesome teams build great products.",
